c6759e357852fe3ddb0b1e52f52744bdd81d8c9a
Sanikookalam <eagle13740@github.com>
2021-02-12 23:23:49 +0330

    ScoreManager

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -7,7 +7,7 @@ public class Rotation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Screen.orientation = ScreenOrientation.Landscape;
     }
 
     // Update is called once per frame
