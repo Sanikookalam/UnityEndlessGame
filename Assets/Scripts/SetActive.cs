f5375a46a695ebd0d0b550508fbc05239e5326ff
Sanikookalam <eagle13740@github.com>
2021-02-13 11:53:42 +0330

    winpage & Restart problem fix

---
+++ Assets/Scripts/SetActive.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SetActive : MonoBehaviour
+{
+    public void active()
+    {
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/WinMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinMenu : MonoBehaviour
+{
+    public string mainMenuLevel;
+
+    public void RestartGame()
+    {
+        
+    }
+
+    public void QuitToMain()
+    {
+        Application.LoadLevel(mainMenuLevel);
+    }
+}
