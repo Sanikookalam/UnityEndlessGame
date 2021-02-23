ac661bcd36518b6faacbe0c9a185a35b918fb18b
Sanikookalam <eagle13740@github.com>
2021-02-23 23:51:02 +0330

    DoshamanCollider Finish

--- Assets/Scripts/DoshmanController.cs
+++ Assets/Scripts/DoshmanController.cs
@@ -9,7 +9,9 @@ public class DoshmanController : MonoBehaviour
     public float xMoveSpeed;
     public float yMoveSpeed;
     public int  s;
-
+    private CircleCollider2D circle;
+    private BoxCollider2D box;
+    private PolygonCollider2D polygon;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +30,19 @@ public class DoshmanController : MonoBehaviour
     {   
         if(other.gameObject.tag == "Player"){
             myRigidbody.velocity = new Vector2(xMoveSpeed,yMoveSpeed);
-            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            if (GetComponent<BoxCollider2D>() != null)
+            {
+                gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            }
+            else if (GetComponent<PolygonCollider2D>() != null)
+            {
+                gameObject.GetComponent<PolygonCollider2D>().enabled = false;
+            }
+            else if (GetComponent<CircleCollider2D>() != null)
+            {
+                gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            }
+
             DeactivateMe();
         }
 
